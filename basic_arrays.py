f540938b7fbb544bca40d54a27bc1805c879ad45
SAHITHICHIRRA <chirrasahithi1@gmail.com>
2026-03-28 20:56:59 +0530

    Create basic_arrays.py
    Creating 1D and 2D arrays using NumPy and print datatype, shape, dimension

---
+++ basic_arrays.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+
+arr1 = np.array([1,2,3,4,5])
+
+
+arr2 = np.array([[1,2,3],
+                 [4,5,6]])
+
+print("1D Array:", arr1)
+print("Datatype:", arr1.dtype)
+print("Shape:", arr1.shape)
+print("Dimension:", arr1.ndim)
+
+print("\n2D Array:", arr2)
+print("Datatype:", arr2.dtype)
+print("Shape:", arr2.shape)
+print("Dimension:", arr2.ndim)
