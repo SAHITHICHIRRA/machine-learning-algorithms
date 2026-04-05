902e03d4c87dee928118a9c6091dd2fcb5472b23
SAHITHICHIRRA <chirrasahithi1@gmail.com>
2026-04-05 22:46:33 +0530

    Create DBSCAN.py

---
+++ DBSCAN.py
@@ -0,0 +1,10 @@
+from sklearn.cluster import DBSCAN
+import numpy as np
+
+X = np.array([[1,2],[2,2],[2,3],
+              [8,7],[8,8],[25,80]])
+
+db = DBSCAN(eps=3, min_samples=2)
+db.fit(X)
+
+print("Cluster Labels:", db.labels_)
